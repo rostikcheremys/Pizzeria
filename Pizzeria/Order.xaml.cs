9f25498beae48c74d5462c02521e0ec52bce929c
rostikсheremys <rostikcheremys@gmail.com>
2024-05-03 14:03:25 +0300

    correct image chicken-with-mushrooms

--- Pizzeria/Order.xaml.cs
+++ Pizzeria/Order.xaml.cs
@@ -1,11 +1,27 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
-namespace Pizzeria;
-
-public partial class Order : Page
+namespace Pizzeria
 {
-    public Order()
+    public partial class Order : Page
     {
-        InitializeComponent();
+        public Order(string name, string imagePath, string ingredients)
+        {
+            InitializeComponent();
+            
+            PizzaName.Content = name;
+            PizzaImage.Source = new System.Windows.Media.Imaging.BitmapImage(new Uri(imagePath, UriKind.Relative));
+            PizzaIngredients.Text = ingredients;
+        }
+
+        private void AddToCart_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Added to Cart");
+        }
+
+        private void OrderNow_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Order placed!");
+        }
     }
 }
--- Pizzeria/Pizza.xaml.cs
+++ Pizzeria/Pizza.xaml.cs
@@ -9,36 +9,64 @@ public partial class Pizza : Page
     {
         InitializeComponent();
     }
-
-    private void OrderPepperoni_Click(object sender, RoutedEventArgs e)
+    
+    private void OrderPepperoni_Click(object sender, RoutedEventArgs e) 
     {
-        MessageBox.Show("OrderPepperoni_Click");
-        Order orderPage = new Order();
+        Order orderPage = new Order(
+            "Pepperoni",
+            "images/pizza/pepperoni.png",
+            "Pepperoni, Chili pepper, Cheese, Tomato Sauce"
+        );
         PizzaPage.Navigate(orderPage);
     }
-    
+
     private void OrderSicilian_Click(object sender, RoutedEventArgs e)
     {
-        MessageBox.Show("OrderSicilian_Click");
+        Order orderPage = new Order(
+            "Sicilian",
+            "images/pizza/sicilian.png", 
+            "Pepperoni, Olives, Tomato, Pepper, Cheese"
+        );
+        PizzaPage.Navigate(orderPage);
     }
-    
+
     private void OrderHawaiian_Click(object sender, RoutedEventArgs e)
     {
-        MessageBox.Show("OrderHawaiian_Click");
+        Order orderPage = new Order(
+            "Hawaiian",
+            "images/pizza/hawaiian.png",
+            "Mozzarella, Chicken, Pineapple, Tomato Sauce"
+        );
+        PizzaPage.Navigate(orderPage);
     }
-    
+
     private void OrderFourCheese_Click(object sender, RoutedEventArgs e)
     {
-        MessageBox.Show("OrderFourCheese_Click");
+        Order orderPage = new Order(
+            "Four Cheese",
+            "images/pizza/four-cheese.png",
+            "Mozzarella, Parmesan, Feta, Gorgonzola"
+        );
+        PizzaPage.Navigate(orderPage);
     }
-    
+
     private void OrderProsciuttoEFunghi_Click(object sender, RoutedEventArgs e)
     {
-        MessageBox.Show("OrderProsciuttoEFunghi_Click");
+        Order orderPage = new Order(
+            "Prosciutto e Funghi",
+            "images/pizza/prosciutto-e-funghi.png",
+            "Ham, Mushrooms, Cheese, Tomato Sauce"
+        );
+        PizzaPage.Navigate(orderPage);
     }
-    
+
     private void OrderChickenWithMushrooms_Click(object sender, RoutedEventArgs e)
     {
-        MessageBox.Show("OrderChickenWithMushrooms_Click");
+        Order orderPage = new Order(
+            "Chicken with Mushrooms",
+            "images/pizza/chicken-with-mushrooms.png",
+            "Chicken, Mushrooms, Tomato, Cheese"
+        );
+        PizzaPage.Navigate(orderPage);
     }
 }
