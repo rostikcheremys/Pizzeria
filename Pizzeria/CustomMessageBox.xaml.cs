632547882a336c757809cd21ce195e56b8dfdbd4
rostikсheremys <rostikcheremys@gmail.com>
2024-05-17 23:55:43 +0300

    adding CustomMessageBox

---
+++ Pizzeria/CustomMessageBox.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace Pizzeria
+{
+    public partial class CustomMessageBox 
+    {
+        public CustomMessageBox(string message, string yesButtonText = "YES", string noButtonText = "NO")
+        {
+            InitializeComponent();
+            
+            MessageText.Text = message;
+            YesButton.Content = yesButtonText;
+            NoButton.Content = noButtonText;
+        }
+
+        private void YesButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void NoButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        public static bool? Show(string message, string yesButtonText = "YES", string noButtonText = "NO")
+        {
+            CustomMessageBox msgBox = new CustomMessageBox(message, yesButtonText, noButtonText);
+            return msgBox.ShowDialog();
+        }
+    }
+}
