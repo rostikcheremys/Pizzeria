bfc67d81e704ab8e6e992d638952fc5df0248484
rostikсheremys <rostikcheremys@gmail.com>
2024-05-23 18:01:11 +0300

    update cart

--- Pizzeria/obj/Debug/net8.0-windows/Pizzeria.AssemblyInfo.cs
+++ Pizzeria/obj/Debug/net8.0-windows/Pizzeria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pizzeria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2712345924bee7d414207b5c3fd362f3633e6b55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69060112fbe357d0d96eead9f36b6784d45a99a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pizzeria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pizzeria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
