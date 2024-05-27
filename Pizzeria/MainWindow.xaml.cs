56ff6fa2b0893b9335c952b6be0fcedb0f858f20
rostikсheremys <rostikcheremys@gmail.com>
2024-05-28 02:20:12 +0300

    update MainWindow

--- Pizzeria/MainWindow.xaml.cs
+++ Pizzeria/MainWindow.xaml.cs
@@ -1,27 +1,30 @@
 ﻿using System.Windows;
 using System.Globalization;
 
-namespace Pizzeria;
-
-public partial class MainWindow
+namespace Pizzeria
 {
-    private readonly Cart _cartPage = new ();
-    private readonly Order _orderPage;
-    public MainWindow()
+    public partial class MainWindow
     {
-        InitializeComponent();
+        private readonly Cart _cartPage = new ();
+        private readonly Order _orderPage;
+        public MainWindow()
+        {
+            InitializeComponent();
         
-        MinWidth = MaxWidth = 1200;
-        MinHeight = MaxHeight = 900;
+            MinWidth = MaxWidth = 1200;
+            MinHeight = MaxHeight = 900;
         
-        Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
-    }
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+        }
     
-    private void MenuButton_Click(object sender, RoutedEventArgs e)
-    {
-        Pizza pizzaPage = new Pizza(_cartPage, _orderPage);
-        MainPage.Navigate(pizzaPage);
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            Pizza pizzaPage = new Pizza(_cartPage, _orderPage);
+            MainPage.Navigate(pizzaPage);
+        }
     }
 }
+
+
 
 
